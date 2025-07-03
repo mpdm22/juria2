ae3d8c41b00658a1763a1702250779e0a40b3fde
mpdm22 <mariepierremarone@gmail.com>
2025-07-03 12:12:33 +0000

    Fix HTML dans st.markdown : fermeture ul manquante

--- app.py
+++ app.py
@@ -77,12 +77,10 @@ st.markdown("""
             <li>📘 Le code de la famille ou du travail </li>
             <li>⚖️ Le code pénal et la procédure pénale</li>
             <li>📄 Les lois, décrets, arrêtés etc.</li>
-        </ul>
     </div>
 """, unsafe_allow_html=True)
 
-
-# -------- CLASSE GROQLLM --------
+# ------------ CLASSE LLM ------------
 class GroqLLM(LLM):
     model: str = "llama-3.3-70b-versatile"
     temperature: float = 0.2
@@ -101,8 +99,17 @@ class GroqLLM(LLM):
         )
         return response.choices[0].message.content
 
-# -------- PROMPT GLOBAL --------
-prompt_template = PromptTemplate.from_template("""
+# ------------ CHARGE LA CHAINE RAG ------------
+@st.cache_resource
+def load_qa_chain():
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="Alibaba-NLP/gte-multilingual-base",
+        model_kwargs={"trust_remote_code": True},
+        encode_kwargs={"normalize_embeddings": True}
+    )
+    db = FAISS.load_local("faiss_index", embedding_model, allow_dangerous_deserialization=True)
+
+    prompt_template = PromptTemplate.from_template("""
 Tu es un assistant juridique spécialisé dans les textes de loi du Sénégal (Code de la famille, Code pénal, décrets, lois, etc).
 
 Ta mission est de répondre de manière claire, concise et fiable à des questions posées par un utilisateur en t'appuyant exclusivement sur les extraits de documents juridiques suivants :
@@ -124,21 +131,8 @@ Consignes strictes :
 Question : {question}
 
 Réponse :
-""")
+    """)
 
-# -------- CACHER UNIQUEMENT LE CHARGEMENT DE FAISS --------
-@st.cache_resource
-def load_faiss_index():
-    embedding_model = HuggingFaceEmbeddings(
-        model_name="Alibaba-NLP/gte-multilingual-base",
-        model_kwargs={"trust_remote_code": True},
-        encode_kwargs={"normalize_embeddings": True}
-    )
-    return FAISS.load_local("faiss_index", embedding_model, allow_dangerous_deserialization=True)
-
-# -------- CHARGER LA CHAÎNE RAG --------
-def load_qa_chain():
-    db = load_faiss_index()
     llm = GroqLLM()
     return RetrievalQA.from_chain_type(
         llm=llm,
@@ -146,7 +140,6 @@ def load_qa_chain():
         return_source_documents=True,
         chain_type_kwargs={"prompt": prompt_template}
     )
-
 
 qa_chain = load_qa_chain()
 
